f1ceb044df1a4e02ab74b194d0b8b0b8acb9e336
Nenad <maksicnenad97@gmail.com>
2020-02-06 20:33:04 +0100

    Initial commit. Created class Car

---
+++ src/main/java/main/factory/Car.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.factory;
+
+/**
+ *
+ * @author qa
+ */
+public class Car {
+    
+    String model;
+    int buildYear;
+    int maxSpeed;
+    String color;
+    int maxFuel;
+    int currentFuel;
+    int consumption;
+    
+    
+    
+}
